d21ad57c0884fb4db0254bcd6c251f92d9ab35d1
Ann-Anyango <annmwangombe@gmail.com>
2023-07-05 23:30:28 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main() {
+    var name="Caroline"
+    println(name)
+    val citizenship="kenyan"
+    var distance=500
+    var wheel=4
+    var pi =3.14150
+    var jackIsKenya="true"
+     var wheels: Byte=4
+    var codehivePop: Short=1254
+    var worldPop: Long=788888809
+    var x:Float =5.279f
+
+//characters
+    var intial='H'
+    var group:String="p"
+
+    //Booleans
+    var sunIOut:Boolean= 
+
+
+
+
+
+}
